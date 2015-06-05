fd02563b9620c8e5ef613100e43c030227dd19d2
Vasa <vasanth.krishnamoorthy@citrix.com>
2015-06-05 14:14:32 -0700

    once()

---
+++ js/once.js
@@ -0,0 +1,28 @@
+/**
+ * once()
+ *
+ * The \function ensure that a given function can be called once and only once, thus preventing duplicate initialization
+ */
+
+(function () {
+  function once(fn, context) {
+    var result;
+
+    return function () {
+      if (fn) {
+        fn.apply(context || this, arguments);
+        fn = null;
+      }
+      return result;
+    };
+  }
+
+  // Usage
+  var canOnlyFireOnce = once(function () {
+    console.log('Fired!');
+  });
+
+  canOnlyFireOnce();  // Fired!
+  canOnlyFireOnce();  // undefined
+
+})();
