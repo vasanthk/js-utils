874426e9f62a7c454831bd730a291fe946519935
Vasa <vasanth.krishnamoorthy@citrix.com>
2015-10-11 10:30:41 -0700

    User Timing API

--- js/measureSitePerformance.js
+++ js/measureSitePerformance.js
@@ -23,7 +23,11 @@
  * Links:
  * http://engineering.tripadvisor.com/html5-navigation-timing/
  * http://www.html5rocks.com/en/tutorials/webperformance/basics/
+ * http://developer.telerik.com/featured/introduction-navigation-timing-api/
  * http://engineering.tripadvisor.com/wp-content/uploads/2015/08/timing-overview.png
+ *
+ * Library:
+ * https://github.com/addyosmani/timing.js/blob/master/timing.js
  */
 
 window.onload = function () {
@@ -32,8 +36,51 @@ window.onload = function () {
     return;
   }
   // Wrap it around a setTimeout to make sure to get it after the load event has ended.
-  setTimeout(function() {
+  setTimeout(function () {
     console.log(window.performance.timing);
     console.log(window.performance.navigation);
   }, 0);
 };
+
+/**
+ * User Timing API
+ * Support: IE10+, Android 4.4+
+ *
+ * Links:
+ * http://www.noupe.com/design/using-html5-apis-measure-performance-with-user-and-navigation-timing-87346.html
+ * http://www.html5rocks.com/en/tutorials/webperformance/usertiming/
+ *
+ * "window.performance.now()” method, you can start measurement at any place of the web application,
+ * for example, within a script that runs a computationally intensive function.
+ * In contrast to the “Date(0)” object, which provides system timing as a time stamp,
+ * the User Timing API gives you millisecond accuracy with several decimal places.
+ * Analyzing the loading performance of an image.
+ */
+
+var measuring_start = window.performance.now();
+var measuring_end;
+document.getElementsByTagName('img')[0].addEventListener('load', function() {
+  measuring_end = window.performance.now();
+  console.log('Image load time: ', measuring_end - measuring_start);
+}, false);
+
+/**
+ * Instead of setting marks with the “now()” method and allocating them a variable,
+ * you can use the “mark()” method and give each mark a specific name.
+ *
+ * “measure()” method then calculates the difference between the two marks.
+ */
+
+window.performance.mark('start');
+window.performance.mark('end');
+window.performance.measure('difference', 'start', 'end');
+
+// To read out the measuring data use the “getEntriesByType()” method. With this method you can determine
+// the names as well as the measured, or rather calculated, timings for the marks set with “mark()” and “measure()”.
+
+var marks = window.performance.getEntriesByType('mark');
+var measuring_differences = window.performance.getEntriesByType('measure');
+
+console.log(mark[0].name + ': ' + mark[0].startTime);
+console.log(mark[1].name + ': ' + mark[1].startTime);
+console.log(measuring_differences[0].name + ': ' + measuring_differences[0].duration);
