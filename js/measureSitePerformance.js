75e096dfeb8b6cdd6634f76971957ae98c751474
Vasa <vasanth.krishnamoorthy@citrix.com>
2015-10-10 22:56:03 -0700

    Navigation Time API

---
+++ js/measureSitePerformance.js
@@ -0,0 +1,20 @@
+/**
+ * Measure site performance using Navigation Timing API
+ *
+ * Note: The last event in navigation timing event is LoadEventEnd,
+ * so you have to wait for the window.onload event to fire before calling any Javascript.
+ * Support: IE9+, Android 4.0+, iOS 9+
+ *
+ * Links:
+ * http://engineering.tripadvisor.com/html5-navigation-timing/
+ * http://engineering.tripadvisor.com/wp-content/uploads/2015/08/timing-overview.png
+ */
+
+window.onload = function () {
+  // Check for browser support
+  if (!(window.performance && window.performance.timing)) {
+    return;
+  }
+
+  console.log(window.performance.timing);
+};
