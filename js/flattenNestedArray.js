f19aed39ba8077e24559fb4938f0148340372e1c
Vasa <vasanth.krishnamoorthy@citrix.com>
2015-10-11 23:08:57 -0700

    Flatten nested array - ES6 and ES7 solns

--- js/flattenNestedArray.js
+++ js/flattenNestedArray.js
@@ -4,10 +4,10 @@
  * Eg. Input array [1, 2, [3, 4, [5, 6]]]
  * On flattening the array it should output [1, 2, 3, 4, 5, 6]
  *
+ * Link: http://stackoverflow.com/a/29158887/1672655
  */
 
-//CASE 1
-
+//CASE 1: Simple and fast!
 (function () {
   var arr = [1, 2, [3, 4, [5, 6]]];
 
@@ -17,7 +17,8 @@
     }
 
     for (var i = 0; i < arr.length; i++) {
-      if (arr[i].constructor === Array) { // Fastest and most effecient way to check if a variable si an array.
+      // Fastest and most effecient way to check if a variable is an array.
+      if (arr[i].constructor === Array) {
         flattenArray(arr[i], result);
       } else {
         result.push(arr[i]);
@@ -30,16 +31,39 @@
 })();
 
 
-//CASE 2
-
-(function(){
-  
+// CASE 2: One liner!
+(function () {
   var arr = [1, 2, [3, 4, [5, 6]]];
-  
+
   function flatten(a) {
     return Array.isArray(a) ? [].concat.apply([], a.map(flatten)) : [a];
   }
-  
+
   console.log(flatten(arr)); // Returns [1, 2, 3, 4, 5, 6]
-    
+
+})();
+
+// CASE 3: Compact with ES6 syntax!
+(function () {
+  var arr = [1, 2, [3, 4, [5, 6]]];
+  var flatten = a => Array.isArray(a) ? [].concat(...a.map(flatten)) : [a];
+
+  console.log(flatten(arr)); // Returns [1, 2, 3, 4, 5, 6]
+})();
+
+// CASE 4: Using ES7 async function (yield)
+(function () {
+  var arr = [1, 2, [3, 4, [5, 6]]];
+
+  function *flatten(a) {
+    if (Array.isArray(a)) {
+      for (var e of a) {
+        yield *flatten(e);
+      }
+    } else {
+      yield a;
+    }
+  }
+
+  console.log(Array.from(flatten(arr))); // Returns [1, 2, 3, 4, 5, 6]
 })();
