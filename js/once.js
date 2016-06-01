58d96d169b048b9b0f44ba8c73e20f07f8c27343
vasanthk <vasanth.walmart@gmail.com>
2016-05-31 23:41:56 -0700

    Code for a function that can be run only once

--- js/once.js
+++ js/once.js
@@ -2,10 +2,25 @@
  * once()
  *
  * The function ensure that a given function can be called once and only once, thus preventing duplicate initialization
- * 
+ *
  * Link: http://sinonjs.org/
  */
 
+// Implementation 1
+(function () {
+  function once(fn) {
+    var returnValue, called = false;
+    return function () {
+      if (!called) {
+        called = true;
+        returnValue = fn.apply(this, arguments);
+      }
+      return returnValue;
+    };
+  }
+})();
+
+// Implementation 2
 (function () {
   function once(fn, context) {
     var result;
